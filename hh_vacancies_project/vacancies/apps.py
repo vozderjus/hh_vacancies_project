6ecc0689167cb3666e861e9d022aad129613e5fa
Nikita Krasikov <memkased@gmail.com>
2025-12-19 20:06:47 +0500

    admin & apps

--- hh_vacancies_project/vacancies/apps.py
+++ hh_vacancies_project/vacancies/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class VacanciesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'vacancies'
+    
+    def ready(self):
+        # Импорт сигналов
+        import vacancies.signals
